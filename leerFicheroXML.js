7a2544debd13127e37d89c9ef1e20a9073a3818d
Manuelbalmis <76203432+Manuelbalmis@users.noreply.github.com>
2021-05-17 09:00:59 +0200

    Update leerFicheroXML.js

--- leerFicheroXML.js
+++ leerFicheroXML.js
@@ -3,7 +3,7 @@ function gestionarFicheroXML(xmlDoc){
 	let libros = xmlDoc.querySelectorAll("libro")
 	for(let i=0; i<libros.length; i++)
 		if(i==3)
-		capaVacia.innerHTML = capaVacia.innerHTML + "<p.class='seleccion'>" + libros[i].textContent + "</p>"
+		capaVacia.innerHTML = capaVacia.innerHTML + "<p.class="seleccion">" + libros[i].textContent + "</p>"
 			else
 				capaVacia.innerHTML = capaVacia.innerHTML + "<p.class="seleccion2">" + libros[i].textContent + "</p>"
 }
