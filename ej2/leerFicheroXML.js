f2cfd6822062d036b9ce2a68bcb138c293742174
Manuelbalmis <76203432+Manuelbalmis@users.noreply.github.com>
2021-05-17 09:45:36 +0200

    Update leerFicheroXML.js

--- ej2/leerFicheroXML.js
+++ ej2/leerFicheroXML.js
@@ -3,9 +3,9 @@ function gestionarFicheroXML(xmlDoc){
 	let entrada = xmlDoc.querySelectorAll("msg")
 	for(let i=0; i<entrada.length; i++)
 		if (i%2==0)
-			capaVacia.innerHTML = capaVacia.innerHTML + "<p class=seleccion>" + entrada[i].textContent + "</p>"
+			capaVacia.innerHTML = capaVacia.innerHTML + "<p align='right' class=seleccion>" + entrada[i].textContent + "</p>"
 			else	
-				capaVacia.innerHTML = capaVacia.innerHTML + "<p class=seleccion2>" + entrada[i].textContent + "</p>"
+				capaVacia.innerHTML = capaVacia.innerHTML + "<p align='left' class=seleccion2>" + entrada[i].textContent + "</p>"
 				
 		
 }
