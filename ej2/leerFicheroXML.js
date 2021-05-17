12293888aabb2ab60124925acc3c3172029cb8a9
Manuelbalmis <76203432+Manuelbalmis@users.noreply.github.com>
2021-05-17 09:18:07 +0200

    Add files via upload

---
+++ ej2/leerFicheroXML.js
@@ -0,0 +1,12 @@
+function gestionarFicheroXML(xmlDoc){
+	let capaVacia = document.querySelector("#ficheroXML")
+	let libros = xmlDoc.querySelectorAll("libro")
+	for(let i=0; i<libros.length; i++)
+		if(i==2)
+		capaVacia.innerHTML = capaVacia.innerHTML + "<p class=seleccion>" + libros[i].textContent + "</p>"
+			else
+				capaVacia.innerHTML = capaVacia.innerHTML + "<p class='seleccion2'>" + libros[i].textContent + "</p>"
+}
+
+loadDocA("libros.xml","xml") ;
+
