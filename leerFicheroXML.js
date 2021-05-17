60a1dbee3a7e0c9736940442e599e0d591830176
carmenFRP <33463902+carmenFRP@users.noreply.github.com>
2021-05-17 09:08:38 +0200

    Create leerFicheroXML.js

--- leerFicheroXML.js
+++ leerFicheroXML.js
@@ -2,8 +2,8 @@ function gestionarFicheroXML(xmlDoc){
 	let capaVacia = document.querySelector("#ficheroXML")
 	let libros = xmlDoc.querySelectorAll("libro")
 	for(let i=0; i<libros.length; i++)
-		if(i==2)
-		capaVacia.innerHTML = capaVacia.innerHTML + "<p class=seleccion>" + libros[i].textContent + "</p>"
+		if(i==3)
+		capaVacia.innerHTML = capaVacia.innerHTML + "<p class='seleccion'>" + libros[i].textContent + "</p>"
 			else
 				capaVacia.innerHTML = capaVacia.innerHTML + "<p class='seleccion2'>" + libros[i].textContent + "</p>"
 }
