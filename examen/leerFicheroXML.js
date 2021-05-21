1c2657c67c5d3c9d31bf61cc41db30ecf19e82e2
Manuelbalmis <76203432+Manuelbalmis@users.noreply.github.com>
2021-05-21 11:27:44 +0200

    Add files via upload

---
+++ examen/leerFicheroXML.js
@@ -0,0 +1,10 @@
+function gestionarFicheroXML(xmlDoc){
+	let capaVacia = document.querySelector("#ficheroXML")
+	let cartel = xmlDoc.querySelectorAll("pelicula")
+	for(let i=0; i<cartel.length; i++)
+		
+		capaVacia.innerHTML = capaVacia.innerHTML + "<p class=seleccion>" + cartel[i].textContent + "</p>"
+			
+}
+
+loadDocA("peliculas.xml","xml") ;
