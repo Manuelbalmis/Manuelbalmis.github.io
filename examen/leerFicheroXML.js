33fda58ce2014bc01e44887c5ab49bbd7ada0b27
Manuelbalmis <76203432+Manuelbalmis@users.noreply.github.com>
2021-05-21 11:57:19 +0200

    Update leerFicheroXML.js

--- examen/leerFicheroXML.js
+++ examen/leerFicheroXML.js
@@ -1,13 +1,14 @@
+
+function CargarFichero()
+{
+	loadDocA("peliculas.xml","xml") ;
+}
+setInterval(CargarFichero,3000);
+
 function gestionarFicheroXML(xmlDoc){
 	let capaVacia = document.querySelector("#ficheroXML")
 	let cartel = xmlDoc.querySelectorAll("pelicula")
 	for(let i=0; i<cartel.length; i++)
 		
 		capaVacia.innerHTML = capaVacia.innerHTML + "<p class=seleccion>" + cartel[i].textContent + "</p>"
-			
-}
-
-function CargarFichero()
-{
-	loadDocA("peliculas.xml","xml") ;
 }
